54dd40a4253002f1639d3841349451ac96aeae89
Jae Doherty <jaeld97@gmail.com>
2021-02-16 22:26:35 -1000

    added long division page

---
+++ src/components/long_division.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LongDivision = function() {
+    return (
+        <div>
+            <h1>Long Division</h1>
+        </div>
+    )
+}
+
+export default LongDivision;
