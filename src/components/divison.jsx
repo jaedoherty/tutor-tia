72b1f08fe5de19142190525d7477f6fdc62cadaf
Jae Doherty <jaeld97@gmail.com>
2021-02-16 22:30:16 -1000

    added home button to all game pages

--- src/components/divison.jsx
+++ src/components/divison.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
 const Division = function() {
     return (
-        <div>
-            <h1>Division Matching</h1>
-        </div>
-    )
+      <div>
+        <h1>Division Matching</h1>
+        <Link to="/">Pick a different game!</Link>
+      </div>
+    );
 }
 
 export default Division;
--- src/components/multiplication.jsx
+++ src/components/multiplication.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState } from "react";
+import {Link} from "react-router-dom";
 
 const FACTORS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 
@@ -62,6 +63,7 @@ const Multiplication = function () {
       </p>
         {createBoard()} 
         {/* <canvas id="multiplication" height="690" width="990"></canvas> */}
+        <Link to ="/">Pick a different game!</Link>
     </div>
   );
 };
