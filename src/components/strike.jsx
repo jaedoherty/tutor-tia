333e010b5a7257ff8b14f4af18b4c5a4e62c5092
Jae Doherty <jaeld97@gmail.com>
2021-03-12 20:38:46 -0800

    reduced renders in strike component

--- src/components/strike.jsx
+++ src/components/strike.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 const Strike = function () {
   const [canvas, setCanvas] = useState("")
-    useEffect(() => {setCanvas({canvas: document.getElementById("strike-canvas")})
+    // useEffect(() => {setCanvas({canvas: document.getElementById("strike-canvas")})
     if (canvas = "") {
       return null
     } else {
